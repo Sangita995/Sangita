191d1d36d72dd14f1fc7bdd6f0fa1fd94b50dac0
sangita <shah20581111@gmail.com>
2025-05-31 20:18:34 +0545

    hello

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+let personName = 'Sangita';
+
+// console.log(personName.toUpperCase());
+// console.log(personName.length)
+// console.log(personName.at(5));
+// console.log(personName.charCodeAt(1));
+// console.log(personName.charCodeAt(1));
+// console.log(personName.codePointAt(90));
+// console.log(personName.concat(' hello jee').concat(' sello jee'));
+// console.log(personName.includes('jee'));
+console.log(personName.split(separator));
